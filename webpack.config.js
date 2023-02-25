ea5ebfc5b7f41f97cac91e0d1444fd52abb5f022
Иван Литовченко <ivan-dp@bk.ru>
2023-02-25 19:58:44 +0300

    project initialization

---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+    mode: "development",
+    stats: {
+        children: false,
+    },
+    devServer: {
+        historyApiFallback: true,
+        open: true,
+        compress: true,
+        hot: true,
+        port: 8080,
+    },
+    entry: {
+        index: "./src/pages/index/index.js",
+    },
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "[name].[hash]bundle.js",
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "./src/pages/index/index.pug",
+            chunks: ["index"],
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new CleanWebpackPlugin(),
+    ],
+    module: {
+        rules: [
+            // JavaScript
+
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            },
+
+            // изображения
+
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg|webp)$/i,
+                loader: "file-loader",
+                options: {
+                    name: "./dist/icons/[name].[ext]",
+                },
+            },
+
+            // шрифты и SVG
+
+            {
+                test: /\.(woff|eot|ttf|svg|)$/,
+                type: "asset/inline",
+            },
+
+            // CSS, PostCSS, Sass
+
+            {
+                test: /\.(scss|css)$/,
+                // exclude: /node_modules/,
+                use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+            },
+            // {
+            //   test: /\.scss$/,
+            //   exclude: /node_modules/,
+            //   use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+            // },
+            // {
+            //   test: /\.css$/,
+            //   use: ["style-loader", "css-loader", "postcss-loader"],
+            // },
+
+            // pug
+
+            {
+                test: /\.pug$/,
+                use: {
+                    loader: "pug-loader",
+                    options: {
+                        pretty: true,
+                    },
+                },
+            },
+        ],
+    },
+};
