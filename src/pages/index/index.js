4b6426c0a1d70d2536573375c7a2244111afafd6
Ivan-dp <ivan-dp@bk.ru>
2023-03-15 11:55:57 +0300

    init noUiSlider

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -10,155 +10,155 @@ $(function () {
 
     // Стоимость автомобиля
 
-    $("#cost-of-car").draggable({
-        animate: "slow",
-        range: "min",
-        value: 3300000,
-        min: 1500000,
-        max: 10000000,
-        step: 100000,
-        slide: function (event, ui) {
-            $("#cost-of-car__value").val(spacesNumbers(ui.value.toString()));
-            $("#initial-payment").draggable("option", "min", Number(ui.value * 0.1));
-            $("#initial-payment").draggable("option", "max", Number(ui.value * 0.6));
-            $("#initial-payment").draggable("option", "value", Number(ui.value * 0.13));
-            $("#initial-payment__value").val(spacesNumbers((ui.value * 0.13).toString()) + " \u20bd");
-            $("#initial-payment__description").val(Math.floor(($("#initial-payment").draggable("value") / $("#cost-of-car").draggable("value")) * 100) + `%`);
-            $("#lease-amount").val(
-                spacesNumbers(
-                    (
-                        Number($("#lease-term").draggable("value")) *
-                            Number(
-                                Math.floor(
-                                    (Number($("#cost-of-car").draggable("value")) - Number($("#initial-payment").draggable("value"))) *
-                                        ((0.05 * Math.pow(1 + 0.05, Number(LEASE_TERM))) / (Math.pow(1 + 0.05, Number($("#lease-term").draggable("value"))) - 1))
-                                )
-                            ) +
-                        Number($("#initial-payment").draggable("value"))
-                    ).toString()
-                ) + " \u20bd"
-            );
-            $("#monthly-payment").val(
-                spacesNumbers(
-                    Math.floor(
-                        (Number($("#cost-of-car").draggable("value")) - Number($("#initial-payment").draggable("value"))) *
-                            ((0.05 * Math.pow(1 + 0.05, Number(LEASE_TERM))) / (Math.pow(1 + 0.05, Number($("#lease-term").draggable("value"))) - 1))
-                    ).toString()
-                ) + " \u20bd"
-            );
-        },
-        classes: {
-            "ui-slider-handle": "custom-handle",
-            "ui-slider-range": "custom-range",
-            main__slider: "custom-slider",
-        },
-    });
+    // $("#cost-of-car").slider({
+    //     animate: "slow",
+    //     range: "min",
+    //     value: 3300000,
+    //     min: 1500000,
+    //     max: 10000000,
+    //     step: 100000,
+    //     slide: function (event, ui) {
+    //         $("#cost-of-car__value").val(spacesNumbers(ui.value.toString()));
+    //         $("#initial-payment").slider("option", "min", Number(ui.value * 0.1));
+    //         $("#initial-payment").slider("option", "max", Number(ui.value * 0.6));
+    //         $("#initial-payment").slider("option", "value", Number(ui.value * 0.13));
+    //         $("#initial-payment__value").val(spacesNumbers((ui.value * 0.13).toString()) + " \u20bd");
+    //         $("#initial-payment__description").val(Math.floor(($("#initial-payment").slider("value") / $("#cost-of-car").slider("value")) * 100) + `%`);
+    //         $("#lease-amount").val(
+    //             spacesNumbers(
+    //                 (
+    //                     Number($("#lease-term").slider("value")) *
+    //                         Number(
+    //                             Math.floor(
+    //                                 (Number($("#cost-of-car").slider("value")) - Number($("#initial-payment").slider("value"))) *
+    //                                     ((0.05 * Math.pow(1 + 0.05, Number(LEASE_TERM))) / (Math.pow(1 + 0.05, Number($("#lease-term").slider("value"))) - 1))
+    //                             )
+    //                         ) +
+    //                     Number($("#initial-payment").slider("value"))
+    //                 ).toString()
+    //             ) + " \u20bd"
+    //         );
+    //         $("#monthly-payment").val(
+    //             spacesNumbers(
+    //                 Math.floor(
+    //                     (Number($("#cost-of-car").slider("value")) - Number($("#initial-payment").slider("value"))) *
+    //                         ((0.05 * Math.pow(1 + 0.05, Number(LEASE_TERM))) / (Math.pow(1 + 0.05, Number($("#lease-term").slider("value"))) - 1))
+    //                 ).toString()
+    //             ) + " \u20bd"
+    //         );
+    //     },
+    //     classes: {
+    //         "ui-slider-handle": "custom-handle",
+    //         "ui-slider-range": "custom-range",
+    //         main__slider: "custom-slider",
+    //     },
+    // });
 
     // Первоначальный взнос
 
-    $("#initial-payment").draggable({
-        animate: "slow",
-        range: "min",
-        value: function (event, ui) {
-            $("#initial-payment").draggable("option", "value", Number(ui.min * 0.13));
-        },
-        step: 1000,
-        slide: function (event, ui) {
-            $("#initial-payment__value").val(spacesNumbers(ui.value.toString()) + " \u20bd");
-            $("#initial-payment__description").val(Math.floor((ui.value / $("#cost-of-car").draggable("value")) * 100) + `%`);
-            $("#initial-payment__description").val(Math.floor(($("#initial-payment").draggable("value") / $("#cost-of-car").draggable("value")) * 100) + `%`);
-            $("#lease-amount").val(
-                spacesNumbers(
-                    (
-                        Number($("#lease-term").draggable("value")) *
-                            Number(
-                                Math.floor(
-                                    (Number($("#cost-of-car").draggable("value")) - Number($("#initial-payment").draggable("value"))) *
-                                        ((0.05 * Math.pow(1 + 0.05, Number(LEASE_TERM))) / (Math.pow(1 + 0.05, Number($("#lease-term").draggable("value"))) - 1))
-                                )
-                            ) +
-                        Number($("#initial-payment").draggable("value"))
-                    ).toString()
-                ) + " \u20bd"
-            );
-            $("#monthly-payment").val(
-                spacesNumbers(
-                    Math.floor(
-                        (Number($("#cost-of-car").draggable("value")) - Number($("#initial-payment").draggable("value"))) *
-                            ((0.05 * Math.pow(1 + 0.05, Number(LEASE_TERM))) / (Math.pow(1 + 0.05, Number($("#lease-term").draggable("value"))) - 1))
-                    ).toString()
-                ) + " \u20bd"
-            );
-        },
-        classes: {
-            "ui-slider-handle": "custom-handle",
-            "ui-slider-range": "custom-range",
-            main__slider: "custom-slider",
-        },
-    });
+    // $("#initial-payment").slider({
+    //     animate: "slow",
+    //     range: "min",
+    //     value: function (event, ui) {
+    //         $("#initial-payment").slider("option", "value", Number(ui.min * 0.13));
+    //     },
+    //     step: 1000,
+    //     slide: function (event, ui) {
+    //         $("#initial-payment__value").val(spacesNumbers(ui.value.toString()) + " \u20bd");
+    //         $("#initial-payment__description").val(Math.floor((ui.value / $("#cost-of-car").slider("value")) * 100) + `%`);
+    //         $("#initial-payment__description").val(Math.floor(($("#initial-payment").slider("value") / $("#cost-of-car").slider("value")) * 100) + `%`);
+    //         $("#lease-amount").val(
+    //             spacesNumbers(
+    //                 (
+    //                     Number($("#lease-term").slider("value")) *
+    //                         Number(
+    //                             Math.floor(
+    //                                 (Number($("#cost-of-car").slider("value")) - Number($("#initial-payment").slider("value"))) *
+    //                                     ((0.05 * Math.pow(1 + 0.05, Number(LEASE_TERM))) / (Math.pow(1 + 0.05, Number($("#lease-term").slider("value"))) - 1))
+    //                             )
+    //                         ) +
+    //                     Number($("#initial-payment").slider("value"))
+    //                 ).toString()
+    //             ) + " \u20bd"
+    //         );
+    //         $("#monthly-payment").val(
+    //             spacesNumbers(
+    //                 Math.floor(
+    //                     (Number($("#cost-of-car").slider("value")) - Number($("#initial-payment").slider("value"))) *
+    //                         ((0.05 * Math.pow(1 + 0.05, Number(LEASE_TERM))) / (Math.pow(1 + 0.05, Number($("#lease-term").slider("value"))) - 1))
+    //                 ).toString()
+    //             ) + " \u20bd"
+    //         );
+    //     },
+    //     classes: {
+    //         "ui-slider-handle": "custom-handle",
+    //         "ui-slider-range": "custom-range",
+    //         main__slider: "custom-slider",
+    //     },
+    // });
 
     // Срок лизинга
 
-    $("#lease-term").draggable({
-        animate: "slow",
-        range: "min",
-        value: 60,
-        min: 6,
-        max: 120,
-        step: 1,
-        slide: function (event, ui) {
-            $("#lease-term__value").val(ui.value);
-            $("#lease-amount").val(
-                spacesNumbers(
-                    (
-                        Number($("#lease-term").draggable("value")) *
-                            Number(
-                                Math.floor(
-                                    (Number($("#cost-of-car").draggable("value")) - Number($("#initial-payment").draggable("value"))) *
-                                        ((0.05 * Math.pow(1 + 0.05, Number(LEASE_TERM))) / (Math.pow(1 + 0.05, Number($("#lease-term").draggable("value"))) - 1))
-                                )
-                            ) +
-                        Number($("#initial-payment").draggable("value"))
-                    ).toString()
-                ) + " \u20bd"
-            );
-            $("#monthly-payment").val(
-                spacesNumbers(
-                    Math.floor(
-                        (Number($("#cost-of-car").draggable("value")) - Number($("#initial-payment").draggable("value"))) *
-                            ((0.05 * Math.pow(1 + 0.05, Number(LEASE_TERM))) / (Math.pow(1 + 0.05, Number($("#lease-term").draggable("value"))) - 1))
-                    ).toString()
-                ) + " \u20bd"
-            );
-        },
-        classes: {
-            "ui-slider-handle": "custom-handle",
-            "ui-slider-range": "custom-range",
-            main__slider: "custom-slider",
-        },
-    });
+    // $("#lease-term").slider({
+    //     animate: "slow",
+    //     range: "min",
+    //     value: 60,
+    //     min: 6,
+    //     max: 120,
+    //     step: 1,
+    //     slide: function (event, ui) {
+    //         $("#lease-term__value").val(ui.value);
+    //         $("#lease-amount").val(
+    //             spacesNumbers(
+    //                 (
+    //                     Number($("#lease-term").slider("value")) *
+    //                         Number(
+    //                             Math.floor(
+    //                                 (Number($("#cost-of-car").slider("value")) - Number($("#initial-payment").slider("value"))) *
+    //                                     ((0.05 * Math.pow(1 + 0.05, Number(LEASE_TERM))) / (Math.pow(1 + 0.05, Number($("#lease-term").slider("value"))) - 1))
+    //                             )
+    //                         ) +
+    //                     Number($("#initial-payment").slider("value"))
+    //                 ).toString()
+    //             ) + " \u20bd"
+    //         );
+    //         $("#monthly-payment").val(
+    //             spacesNumbers(
+    //                 Math.floor(
+    //                     (Number($("#cost-of-car").slider("value")) - Number($("#initial-payment").slider("value"))) *
+    //                         ((0.05 * Math.pow(1 + 0.05, Number(LEASE_TERM))) / (Math.pow(1 + 0.05, Number($("#lease-term").slider("value"))) - 1))
+    //                 ).toString()
+    //             ) + " \u20bd"
+    //         );
+    //     },
+    //     classes: {
+    //         "ui-slider-handle": "custom-handle",
+    //         "ui-slider-range": "custom-range",
+    //         main__slider: "custom-slider",
+    //     },
+    // });
 
-    $("#cost-of-car__value").val($("#cost-of-car").draggable("value"));
+    $("#cost-of-car__value").val($("#cost-of-car").slider("value"));
     $("#cost-of-car__description").val(`\u20bd`);
-    $("#initial-payment__value").val(spacesNumbers(($("#cost-of-car").draggable("value") * 0.13).toString()) + " \u20bd");
-    $("#initial-payment").draggable("option", "min", Number($("#cost-of-car").draggable("value") * 0.1));
-    $("#initial-payment").draggable("option", "value", Number($("#cost-of-car").draggable("value") * 0.13));
-    $("#initial-payment").draggable("option", "max", Number($("#cost-of-car").draggable("value") * 0.6));
-    $("#initial-payment__description").val(Math.floor(($("#initial-payment").draggable("value") / $("#cost-of-car").draggable("value")) * 100) + `%`);
-    $("#lease-term__value").val($("#lease-term").draggable("value"));
+    $("#initial-payment__value").val(spacesNumbers(($("#cost-of-car").slider("value") * 0.13).toString()) + " \u20bd");
+    $("#initial-payment").slider("option", "min", Number($("#cost-of-car").slider("value") * 0.1));
+    $("#initial-payment").slider("option", "value", Number($("#cost-of-car").slider("value") * 0.13));
+    $("#initial-payment").slider("option", "max", Number($("#cost-of-car").slider("value") * 0.6));
+    $("#initial-payment__description").val(Math.floor(($("#initial-payment").slider("value") / $("#cost-of-car").slider("value")) * 100) + `%`);
+    $("#lease-term__value").val($("#lease-term").slider("value"));
     $("#lease-term__description").val("мес.");
 
     var LEASE_TERM = $("#lease-term__value").val();
     var MONTHLY_PAYMENT = Math.floor(
-        (Number($("#cost-of-car").draggable("value")) - Number($("#initial-payment").draggable("value"))) *
-            ((0.05 * Math.pow(1 + 0.05, Number(LEASE_TERM))) / (Math.pow(1 + 0.05, Number($("#lease-term").draggable("value"))) - 1))
+        (Number($("#cost-of-car").slider("value")) - Number($("#initial-payment").slider("value"))) *
+            ((0.05 * Math.pow(1 + 0.05, Number(LEASE_TERM))) / (Math.pow(1 + 0.05, Number($("#lease-term").slider("value"))) - 1))
     );
 
-    $("#lease-amount").val(spacesNumbers((Number($("#lease-term").draggable("value")) * Number(MONTHLY_PAYMENT) + Number($("#initial-payment").draggable("value"))).toString()) + " \u20bd");
+    $("#lease-amount").val(spacesNumbers((Number($("#lease-term").slider("value")) * Number(MONTHLY_PAYMENT) + Number($("#initial-payment").slider("value"))).toString()) + " \u20bd");
 
     var COST_CAR = $("#cost-of-car__value").val();
     var INITIAL_PAYMENT = $("#initial-payment__value").val();
-    var LEASE_AMOUNT__VALUE = Number($("#lease-term").draggable("value")) * Number(MONTHLY_PAYMENT) + Number($("#initial-payment").draggable("value"));
+    var LEASE_AMOUNT__VALUE = Number($("#lease-term").slider("value")) * Number(MONTHLY_PAYMENT) + Number($("#initial-payment").slider("value"));
 
     COST_CAR = spacesNumbers(COST_CAR.toString());
     INITIAL_PAYMENT = spacesNumbers(INITIAL_PAYMENT.toString());
@@ -188,4 +188,47 @@ $(function () {
         $("#request-button").attr("disabled", "true");
         alert(JSON.stringify(REQUEST_OBJECT, null, 2));
     });
+});
+
+import noUiSlider from "nouislider";
+import "nouislider/dist/nouislider.css";
+
+console.log("IT WORKS!!!");
+
+const costOfCar = document.querySelector("#cost-of-car");
+const initPayment = document.querySelector("#initial-payment");
+const leaseTerm = document.querySelector("#lease-term");
+
+noUiSlider.cssClasses.connect += " custom-range";
+noUiSlider.cssClasses.handle += " custom-handle";
+noUiSlider.cssClasses.touchArea += " custom-handle";
+
+noUiSlider.create(costOfCar, {
+    behaviour: "tap",
+    connect: [true, false],
+    start: 3300000,
+    range: {
+        min: 1500000,
+        max: 10000000,
+    },
+});
+
+noUiSlider.create(initPayment, {
+    behaviour: "tap",
+    connect: [true, false],
+    start: 3300000,
+    range: {
+        min: 1500000,
+        max: 10000000,
+    },
+});
+
+noUiSlider.create(leaseTerm, {
+    behaviour: "tap",
+    connect: [true, false],
+    start: 3300000,
+    range: {
+        min: 1500000,
+        max: 10000000,
+    },
 });
